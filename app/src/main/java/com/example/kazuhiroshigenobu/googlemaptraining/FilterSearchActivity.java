2c439cf353405d65e7dd58219087233a9541cd82
重信和宏 <kazushige359@gmail.com>
2017-02-23 22:49:44 +1100

    I am going to add a toolbar for the filter search activity.

--- app/src/main/java/com/example/kazuhiroshigenobu/googlemaptraining/FilterSearchActivity.java
+++ app/src/main/java/com/example/kazuhiroshigenobu/googlemaptraining/FilterSearchActivity.java
@@ -1,8 +1,13 @@
 package com.example.kazuhiroshigenobu.googlemaptraining;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -12,19 +17,56 @@ import android.widget.Toast;
 
 public class FilterSearchActivity extends AppCompatActivity {
 
+//    Toolbar toolbar;
     Spinner spinner1;
+    Spinner spinner2;
+    Spinner spinner3;
+    Spinner spinner4;
     ArrayAdapter<CharSequence> adapter1;
+    ArrayAdapter<CharSequence> adapter2;
+    ArrayAdapter<CharSequence> adapter3;
+    ArrayAdapter<CharSequence> adapter4;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter_search);
+
+//        toolbar = (Toolbar) findViewById(R.id.app_bar2);
+//        toolbar.setNavigationIcon(R.drawable.earth);
+//        setSupportActionBar(toolbar);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//
+//        toolbar.setNavigationOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v) {
+//                Log.i("YouareThebest", "11");
+//                Intent intent = new Intent(v.getContext(),MapsActivity.class);
+//                startActivity(intent);
+//
+//             }
+//        }
+//        );
+
         spinner1 = (Spinner) findViewById(R.id.spinner1);
+        spinner2 = (Spinner) findViewById(R.id.spinner2);
+        spinner3 = (Spinner) findViewById(R.id.spinner3);
+        spinner4 = (Spinner) findViewById(R.id.spinner4);
         adapter1 = ArrayAdapter.createFromResource(this,R.array.distance_names,android.R.layout.simple_spinner_item);
-        //Changed (this,R.array.distance_names,android.R.layout.simple_spinner_item)
+        adapter2 = ArrayAdapter.createFromResource(this,R.array.order_names,android.R.layout.simple_spinner_item);
+        adapter3 = ArrayAdapter.createFromResource(this,R.array.places_names,android.R.layout.simple_spinner_item);
+        adapter4 = ArrayAdapter.createFromResource(this,R.array.star_numbers,android.R.layout.simple_spinner_item);
+
 
         adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner1.setAdapter(adapter1);
+        spinner2.setAdapter(adapter2);
+        spinner3.setAdapter(adapter3);
+        spinner4.setAdapter(adapter4);
         spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -43,5 +85,103 @@ public class FilterSearchActivity extends AppCompatActivity {
             }
         });
 
+        spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                ((TextView) parent.getChildAt(0)).setTextColor(Color.BLUE);
+                ((TextView) parent.getChildAt(0)).setTextSize(20);
+                ((TextView) parent.getChildAt(0)).setText(String.valueOf(parent.getItemAtPosition(position)));
+                //Commented 8pm 23th
+
+                Toast.makeText(getBaseContext(),parent.getItemAtPosition(position) + "Selected",Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        spinner3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                ((TextView) parent.getChildAt(0)).setTextColor(Color.BLUE);
+                ((TextView) parent.getChildAt(0)).setTextSize(20);
+                ((TextView) parent.getChildAt(0)).setText(String.valueOf(parent.getItemAtPosition(position)));
+
+
+                Toast.makeText(getBaseContext(),parent.getItemAtPosition(position) + "Selected",Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        spinner4.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                ((TextView) parent.getChildAt(0)).setTextColor(Color.BLUE);
+                ((TextView) parent.getChildAt(0)).setTextSize(20);
+                ((TextView) parent.getChildAt(0)).setText(parent.getItemAtPosition(position) + "以上を検索");
+
+
+                Toast.makeText(getBaseContext(),parent.getItemAtPosition(position) + "Selected",Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
     }
+
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//
+//
+//        getMenuInflater().inflate(R.menu.filter, menu);
+//        //Commented for adding below code at 5pm
+//        //getMenuInflater().inflate(R.menu.filter,amvMenu.getMenu());
+//
+//
+////        MenuInflater inflater = getMenuInflater();
+////        inflater.inflate(R.menu.menufile);
+//        return super.onCreateOptionsMenu(menu);
+//    }
+//
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        int id = item.getItemId();
+//
+//        if (id == R.id.account){
+//            Toast.makeText(this, "Hey Did you Click Account??", Toast.LENGTH_SHORT).show();
+//            return  true;
+//
+//        } else
+//
+//
+//
+//        if (id == R.id.filter){
+//            Toast.makeText(this, "Hey Did you Click filter??", Toast.LENGTH_SHORT).show();
+//            return  true;
+//
+//        } else {
+//
+//            Toast.makeText(this, String.valueOf(id), Toast.LENGTH_SHORT).show();
+//
+////        if id == R.id.
+//            return super.onOptionsItemSelected(item);
+//        }
+//    }
+//
+
+
 }
